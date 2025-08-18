05dec7e19f8888a69840ae870915e74d329dbf4b
Cam <cam.soper@outlook.com>
2025-08-18 12:43:50 -0500

    Refactor Zigbee2Mqtt device configuration for improved readability

--- home-lab-iac/ContainerConfigs/IotGateway/Zigbee2Mqtt.cs
+++ home-lab-iac/ContainerConfigs/IotGateway/Zigbee2Mqtt.cs
@@ -27,7 +27,8 @@ public class Zigbee2Mqtt : ContainerConfigBase
             }
         };
         ContainerArgs.Devices = new InputList<ContainerDeviceArgs> {
-            new ContainerDeviceArgs {
+            new ContainerDeviceArgs
+            {
                 HostPath = "/dev/serial/by-id/usb-ITead_Sonoff_Zigbee_3.0_USB_Dongle_Plus_d4bf08e152c9eb1186ae914f1d69213e-if00-port0",
                 ContainerPath = "/dev/ttyUSB0"
             }
